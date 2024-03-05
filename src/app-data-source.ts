561ada933466a066796c04d48d8d30e0231fa85b
MTthoas <matthias.pecquery@gmail.com>
2024-03-05 06:30:18 +0100

    synchro

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     type: 'postgres',
-    synchronize: false,
+    synchronize: true,
     logging: false,
     entities: [Feedback, User, Trip],
     // migrations: ['src/migrations/**/*{.ts,.js}'],
